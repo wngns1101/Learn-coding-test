08b35964208a490a7074f7b11e4c088edc744d22
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 <wngns0812@gmail.com>
2023-06-15 11:04:04 +0900

    remove: python

--- ch1/ex1.py
+++
@@ -1,9 +0,0 @@
-# 시간 복잡도 예제 O(N)표기법
-
-array = [3, 5, 1, 7, 4]
-summary = 0
-
-for x in array:
-    summary += x
-
-print(summary)
--- ch1/ex3.py
+++
@@ -1,9 +0,0 @@
-# 시간 복잡도 예제 O(N*N)표기법
-
-array = [3, 5, 1, 2, 4]
-temp = 0
-
-for i in array:
-    for j in array:
-        temp = i * j
-        print(temp)
