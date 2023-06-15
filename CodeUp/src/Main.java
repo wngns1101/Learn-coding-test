4c09b52af0dffba5b7f8ad1cb4f491a5fe102cba
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 <wngns0812@gmail.com>
2023-06-15 14:57:34 +0900

    feat: 코드업 기초 100제(1001~1010)

---
+++ CodeUp/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+/*
+        1001
+        System.out.printf("Hello");
+
+        1002
+        System.out.printf("Hello World");
+
+        1003
+        System.out.println("Hello");
+        System.out.printf("World");
+
+        1004
+        System.out.printf("'Hello'");
+
+        1005
+        System.out.printf("\"Hello World\"");
+
+        1006
+        System.out.println("!@#$%^&*()");
+
+        1007
+        System.out.println("C:\\Download\\hello.java");
+
+        1008
+        System.out.println("\u250C\u252C\u2510");
+        System.out.println("\u251C\u253C\u2524");
+        System.out.println("\u2514\u2534\u2518");
+
+        1010
+        int i;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        i = Integer.parseInt(br.readLine());
+        System.out.println(i);
+*/
+
+    }
+}
