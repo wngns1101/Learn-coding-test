6df7260b5e5328168e9a6626bddec074f0672622
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 <wngns0812@gmail.com>
2023-06-19 13:28:59 +0900

    feat: 코드업 기초 100제(1011~1020)

--- CodeUp/src/Main.java
+++ CodeUp/src/Main.java
@@ -37,7 +37,51 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         i = Integer.parseInt(br.readLine());
         System.out.println(i);
-*/
 
+        1011
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+        System.out.println(c);
+
+        1012
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        float f = Float.parseFloat(br.readLine());
+        System.out.println(f);
+
+        1013
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(str[0])+ " " + Integer.parseInt(str[1]));
+
+        1014
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = br.readLine().split(" ");
+        System.out.println(str[1] + " " + str[0]);
+
+        1015
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        float num = Float.parseFloat(br.readLine());
+        System.out.printf("%.3f", num);
+
+        1017
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(num + " " + num + " " + num);
+
+        1018
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String time[] = br.readLine().split(":");
+        System.out.println(time[0] + ":" + time[1]);
+
+        1019
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = br.readLine().split("[.]");
+        System.out.println(str[0] + ".0" + str[1] + ".0" + str[2]);
+
+        1020
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = br.readLine().split("-");
+        System.out.println(str[0] + str[1]);
+ */
     }
 }
