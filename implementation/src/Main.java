0467125c9f7393f92ac3b4021a2e6734a94728b0
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 <wngns0812@gmail.com>
2023-07-08 20:40:02 +0900

    feat: 구현 알고리즘 예제(1~2)

---
+++ implementation/src/Main.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+//        3-1
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int space = Integer.parseInt(br.readLine());
+//        int x = 1;
+//        int y = 1;
+//        String coordinate[] = br.readLine().split(" ");
+//        for (String splitCoordinate : coordinate) {
+//            if (splitCoordinate.equals("R")) {
+//                if (y >= 5) {
+//                    y = 5;
+//                }else{
+//                    y++;
+//                }
+//            } else if (splitCoordinate.equals("U")) {
+//                if (x <= 1) {
+//                    x = 1;
+//                }else{
+//                    x--;
+//                }
+//            }else if (splitCoordinate.equals("L")){
+//                if (y <= 1) {
+//                    y = 1;
+//                }else{
+//                    y--;
+//                }
+//            }else{
+//                if (x >= 5) {
+//                    x = 5;
+//                }else{
+//                    x++;
+//                }
+//            }
+//        }
+//        System.out.println(x + " " + y);
+//        3-2
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int hour = Integer.parseInt(br.readLine());
+//        int count = 0;
+//        for (int h = 0; h <= hour; h++) {
+//            for (int m = 1; m <= 60; m++) {
+//                for (int s = 1; s <= 60; s++) {
+//                    String str = Integer.toString(h) + Integer.toString(m) + Integer.toString(s);
+//                    if (str.contains("3")) {
+//                        count += 1;
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println(count);
+    }
+}
